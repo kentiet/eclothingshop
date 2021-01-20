8bae032b35232f8e0a2fc83593d5c77992e740f6
Ken Tiet <ken.tietkien@gmail.com>
2021-01-19 22:28:12 -0800

    add more components and styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 
+import HomePage from './components/pages/homepage/homepage.component'
+
 function App() {
   return (
     <div>
-      Hello World
+      <HomePage />
     </div>
   );
 }
