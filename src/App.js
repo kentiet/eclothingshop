abbd8986c253949edeaac2c9df9675abc3515581
Ken Tiet <ken.tietkien@gmail.com>
2021-02-04 21:07:53 -0800

    add firebase config

--- src/App.js
+++ src/App.js
@@ -6,18 +6,37 @@ import HomePage from './components/pages/homepage/homepage.component'
 import ShopPage from './components/pages/shop/shop.component'
 import Header from './components/header/header.component'
 import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import { auth } from "../src/components/firebase/firebase.util";
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/signIn' component={SignInAndSignUpPage} />
-      </Switch>
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  componentDidMount() {
+    auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user})
+      console.log(user)
+    })
+  }
+
+  render () {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signIn' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
+
 }
 
 export default App;
